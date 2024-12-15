fb9446e5d2354c278087a590d0feb123e2f6e1a1
smiff0009 <trevorgustaveson@gmail.com>
2024-12-15 16:09:39 -0700

    add dom.js

---
+++ dom.js
@@ -0,0 +1,30 @@
+// Variables
+
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+const quotes = [{
+    quote: `"You miss 100 percent of the shots you don't take."`,
+    person: ` Michael Jordan`
+}, {
+    quote: `"Bitches ain't shit but hoes and tricks."`,
+    person: ` Dr. Dre`
+}, {
+    quote: `"That's what I said - I said I'm setting booby traps!"`,
+    person: ` Data`
+}, {
+    quote: `"A job you love and you will never have."`,
+    person: ` Confucius`
+}, {
+    quote: `"You will be more disappointed."`,
+    person: ` Mark Twain`
+}, ];
+
+btn.addEventListener('click', function(){
+
+    let random = Math.floor(Math.random() * quotes.length);
+
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
+})
